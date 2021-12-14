c36074d5b5518df46c4e03307d8ad106f8302b36
kathycxz <54104884+kathycxz@users.noreply.github.com>
2021-12-14 19:28:53 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,4 @@
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 git clone https://github.com/iwrt/luci-app-ikoolproxy.git package/luci-app-ikoolproxy
+git clone https://github.com/rosywrt/luci-theme-rosy.git package/luci-theme-rosy
